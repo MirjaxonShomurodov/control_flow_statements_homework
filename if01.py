6600ca8998504bb21aa83566b4aca6a3dab8d2ad
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-17 08:49:06 +0500

    1 misol

--- if01.py
+++ if01.py
@@ -1,9 +1,14 @@
 def main(a):
-    """
+    if a>0:
+        return (a+1)
+print(main(2))
+
+"""
     If the number is positive, increase it to 1,otherwise leave unchanged.
     Args:
         a: integer
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+
+
