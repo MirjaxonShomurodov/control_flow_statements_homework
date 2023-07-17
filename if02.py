8e7e0b3fcf9ed7bd88b6ba16d32fa31f67e03b24
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-17 08:53:45 +0500

    2 misol

--- if02.py
+++ if02.py
@@ -1,9 +1,13 @@
 def main(a):
-    """
+    if a>0:
+        return (a+1)
+    else:
+        return(a-2)
+print(main(-4))
+"""
     If the number is positive, increase it to 1,otherwise decrease it to 2.
     Args:
         a: integer
     Returns:
         a: a increased by 1 if positive, else decreased by 2.
     """
-    return
