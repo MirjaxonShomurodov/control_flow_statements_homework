0dc32666d1f8b7d20183933c1dbfeaaa7133ccce
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-18 13:33:46 +0500

    6 misol

--- if06.py
+++ if06.py
@@ -1,4 +1,5 @@
 def main(a,b,c):
+    
     """
     Find how many positive and how many negative numbers there are in the given numbers.
     check the following conditions:
@@ -13,4 +14,18 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    if a>0 and b>0 and c>0:
+        return "there are a lot of positive numbers"
+    elif (a>0 and b>0 and c<0 ) or (a>0 and b<0 and c>0) or (a<0 and b>0 and c>0):
+        return "there are a lot of positive numbers"
+    elif (a>0 and b<0 and c<0) or (a<0 and b>0 and c<0) or (a<0 and b<0 and c>0):
+        return "there are a lot of positive numbers"
+    if a<0 and b<0 and c<0:
+        return "there are a lot of negative numbers"
+    elif (a<0 and b<0 and c>0 ) or (a<0 and b>0 and c<0) or (a>0 and b<0 and c<0):
+        return "there are a lot of negative numbers"
+    elif (a>0 and b>0 and c<0) or (a>0 and b<0 and c>0) or (a<0 and b>0 and c>0):
+        return "there are a lot of negative numbers"
+    else:
+        return 0
+print(main(-2,4,5))
