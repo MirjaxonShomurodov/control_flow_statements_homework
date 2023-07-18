dd27dd6893331c2199481ff3e0b97aae0f397ad3
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-18 13:53:11 +0500

    9 misol

--- if09.py
+++ if09.py
@@ -1,5 +1,14 @@
 def main(a):
-    """
+    x=a//10
+    y=a%10
+    b=y*10+x
+    if  (a>b or a==b):
+        return True
+    else:
+        return False
+print(main(13))
+
+"""
     The two-digit integer is given.
     Replace the digits of the number.
     True if the resulting number is less than or equal to the old number, otherwise return False.
@@ -9,4 +18,3 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
