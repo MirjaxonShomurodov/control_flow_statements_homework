b5c4ed230ad035022896d0c5667d9289a2708ba4
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-18 11:00:58 +0500

    10 misol

--- if10.py
+++ if10.py
@@ -1,5 +1,18 @@
 def main(temp):
-    """
+    if temp<0:
+        return "Freezing"
+    if temp>1 and temp<10:
+        return "Very Cold"
+    if temp>11 and temp<20:
+        return "Cold"
+    if temp>21 and temp<30:
+        return "Normal"
+    if temp>31 and temp<40:
+        return "Hot"
+    if temp>40:
+        return "Very Hot"
+print(main(32))
+"""
     Display the message according to the following temperature conditions given to you in Celsius:
     Temp<0: "Freezing"
     Temp 1-10: "Very Cold"
@@ -13,4 +26,3 @@ def main(temp):
     Returns:
         string: the message to print
     """
-    return
