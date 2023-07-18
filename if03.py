345c3a9c57703774634b44a419433292b6813286
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-18 08:09:52 +0500

    3 misol

--- if03.py
+++ if03.py
@@ -1,5 +1,13 @@
 def main(a):
-    """
+    if a>0:
+        return (a+1)
+    if (a<0):
+        return (a-2)
+    else:
+     (a==0)
+    return (10)
+print(main(0))
+"""
     If the number is positive, increase it to 1,if the number is negative decrease it to 2.
     If it is 0, assign 10.
     Args:
@@ -7,4 +15,3 @@ def main(a):
     Returns:
         a: integer
     """
-    return
